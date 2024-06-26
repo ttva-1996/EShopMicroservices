dec1a0db5e8063190a845584b4d2723ca8baa95f
anh.ton <anh.ton@aegona.com>
2024-06-26 22:48:29 +0700

    commit lesson 60

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,20 +1,15 @@
-﻿using BuildingBlocks.CQRS;
-
-using Catalog.API.Models;
-
-namespace Catalog.API.Products.CreateProduct;
+﻿namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
     : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler 
+internal class CreateProductCommandHandler(IDocumentSession session)
     : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
         //create Product entity from command object
-
         var product = new Product
         {
             Name = command.Name,
@@ -24,10 +19,11 @@ internal class CreateProductCommandHandler
             Price = command.Price,
         };
 
-        //TODO
         //save to database
-        //return CreateProductResult result
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
 
-        return new CreateProductResult(Guid.NewGuid());
+        //return result
+        return new CreateProductResult(product.Id);
     }
 }
