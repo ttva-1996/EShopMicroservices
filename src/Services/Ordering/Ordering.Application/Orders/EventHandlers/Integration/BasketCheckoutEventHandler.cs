58e7808c827a2915fc4bd9405ee69df1de373e1d
anh.ton <anh.ton@aegona.com>
2024-07-05 15:05:48 +0700

    commit lesson 286

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Integration/BasketCheckoutEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using BuildingBlocks.Messaging.Events;
+
+using MassTransit;
+
+namespace Ordering.Application.Orders.EventHandlers.Integration;
+
+public class BasketCheckoutEventHandler
+    (ISender sender, ILogger<BasketCheckoutEventHandler> logger)
+    : IConsumer<BasketCheckoutEvent>
+{
+    public Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+    {
+        throw new NotImplementedException();
+    }
+}
